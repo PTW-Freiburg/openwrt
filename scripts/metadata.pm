2ddcbfe686bfac766ea14915a87a07371ee7fb49
Scott Kempf <scott.kempf@ptwdosimetry.com>
2026-03-20 17:21:42 +0100

    package-dumpinfo,metadata: add src-name and build-dir

--- scripts/metadata.pm
+++ scripts/metadata.pm
@@ -255,6 +255,8 @@ sub parse_package_metadata($) {
 		/^Submenu: \s*(.+)\s*$/ and $pkg->{submenu} = $1;
 		/^Submenu-Depends: \s*(.+)\s*$/ and $pkg->{submenudep} = $1;
 		/^Source: \s*(.+)\s*$/ and $pkg->{source} = $1;
+		/^Source-Name: \s*(.+)\s*$/ and $pkg->{sourcename} = $1;
+		/^Build-Dir: \s*(.+)\s*$/ and $pkg->{builddir} = $1;
 		/^Source-URL: \s*(.+)\s*$/ and $pkg->{sourceurl} = $1;
 		/^Source-Protocol: \s*(.+)\s*$/ and $pkg->{sourceproto} = $1;
 		/^License: \s*(.+)\s*$/ and $pkg->{license} = $1;
